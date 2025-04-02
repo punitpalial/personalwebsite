09a2f346e19a9b7a8cb15c3a7c2b1118437a1f65
punitpalial <punit.palial@gmail.com>
2025-04-02 14:01:12 +0530

    Before experimenting with React. Website works just fine right now

---
+++ website/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+    return (
+        <>
+        <div >
+        <h1>My Website</h1>
+        <p>Welcome to my website!</p>
+        <form>
+            <label>
+              
+                <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+        </div>
+        </>
+    );
+}
+
+export default App;
